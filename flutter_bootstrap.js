138fb6207db668c4dfbe0c0fa7d9b2a25261e0de
git config --global user.name linuxxb <ghbartwlyd@gmail.com>
2025-03-26 06:37:05 -0700

    Resolve merge conflicts

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3472681383"
+    serviceWorkerVersion: "1188536545"
   }
 });
